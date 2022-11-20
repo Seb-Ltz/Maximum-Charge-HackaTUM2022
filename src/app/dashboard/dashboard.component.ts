13455aa15b535d4624d825f831dfbc348b8d5657
Sébastien Letzelter <sebastien.letzelter@gmail.com>
2022-11-20 01:12:43 +0100

    removed mocked debug data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,63 +24,6 @@ export class DashboardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.locationService.currentLocation = {
-      id: 'poi.678604846861',
-      type: 'Feature',
-      place_type: ['poi'],
-      relevance: 1,
-      properties: {
-        foursquare: '4bfe36a8e9309521203462ab',
-        landmark: true,
-        address: '3.01 The Plaza, 535 Kings Rd.',
-        category: 'design, office, design office, business, service',
-      },
-      text: 'Hello World Group',
-      place_name:
-        'Hello World Group, 3.01 The Plaza, 535 Kings Rd., London, England SW10 0SZ, United Kingdom',
-      center: [-0.186274, 51.479934],
-      geometry: {
-        coordinates: [-0.186274, 51.479934],
-        type: 'Point',
-      },
-      context: [
-        {
-          id: 'neighborhood.16944207',
-          text: 'Fulham',
-        },
-        {
-          id: 'postcode.12100939343',
-          text: 'SW10 0SZ',
-        },
-        {
-          id: 'locality.95816271',
-          text: 'Kensington and Chelsea',
-        },
-        {
-          id: 'place.6957135',
-          wikidata: 'Q84',
-          text: 'London',
-        },
-        {
-          id: 'district.591439',
-          wikidata: 'Q23306',
-          text: 'Greater London',
-        },
-        {
-          id: 'region.9295',
-          short_code: 'GB-ENG',
-          wikidata: 'Q21',
-          text: 'England',
-        },
-        {
-          id: 'country.8783',
-          short_code: 'gb',
-          wikidata: 'Q145',
-          text: 'United Kingdom',
-        },
-      ],
-    };
-
     if (!this.locationService.currentLocation) {
       this.router.navigate(['/']);
     }
